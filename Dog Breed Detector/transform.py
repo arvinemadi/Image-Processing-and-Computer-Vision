386b2a03a7fc410c7936fecda261d67a69e986c9
Arvin Emadi <arvin.emadi@gmail.com>
2022-09-16 07:47:05 -0500

    Update transform.py

--- Dog Breed Detector/transform.py
+++ Dog Breed Detector/transform.py
@@ -6,14 +6,10 @@ import torchvision.models as models
 from torchvision import transforms
 from PIL import Image
 
-
-
 def model_fn(model_dir):
     model = models.resnet18(pretrained=True)
-
     for param in model.parameters():
         param.requires_grad = False
-
     num_features = model.fc.in_features
     model.fc = nn.Sequential(
                             nn.Linear(num_features, 256),
@@ -25,12 +21,12 @@ def model_fn(model_dir):
         model.load_state_dict(torch.load(f))
     return model
 
-
 def input_fn(request_body, content_type):
     image = Image.open(io.BytesIO(request_body))
 
     transformation = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor()])
+                                        transforms.Resize((224, 224)),
+                                        transforms.ToTensor()
+                                        ])
 
     return transformation(image).unsqueeze(0)
